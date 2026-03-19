04e719c1a4706b31ec9620cec982d2c4719b26a8
Arthur Mello Pimentel <Arthurmellopimentel76@gmail.com>
2026-03-19 23:38:06 +0000

    codigoinicialC

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int valor1, valor2;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &valor1);
+
+    printf("Digite o segundo valor: ");
+    scanf("%d", &valor2);
+
+    printf("Você digitou: %d e %d\n", valor1, valor2);
+
+    return 0;
+}
